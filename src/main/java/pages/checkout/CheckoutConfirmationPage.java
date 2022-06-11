baf45d604e47f325e8b6f4b0626dbf54cbc8a0ed
Mateusz Tadla <mta@scalepoint.com>
2022-06-11 15:55:44 +0200

    order configmation page

--- src/main/java/pages/checkout/CheckoutConfirmationPage.java
+++ src/main/java/pages/checkout/CheckoutConfirmationPage.java
@@ -1,10 +1,28 @@
 package pages.checkout;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import pages.base.BasePage;
 
 public class CheckoutConfirmationPage extends BasePage {
     public CheckoutConfirmationPage(WebDriver driver) {
         super(driver);
     }
+
+    @FindBy(css = "h1")
+    private WebElement orderConfirmationMsg;
+
+    @FindBy(xpath = "//div[@id='order-details']//li[1]")
+    private WebElement orderNumber;
+
+    public String getOrderNumber() {
+        return orderNumber.getText().replace("Order reference: ", "");
+    }
+
+    public String getOrderConfirmationMsg() {
+        return orderConfirmationMsg.getText();
+    }
+
+
 }
