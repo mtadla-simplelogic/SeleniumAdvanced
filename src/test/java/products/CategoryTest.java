733262499f8b073cffd53d8a3457e990e5326407
Mateusz Tadla <mta@scalepoint.com>
2022-06-12 09:39:48 +0200

    category test

---
+++ src/test/java/products/CategoryTest.java
@@ -0,0 +1,16 @@
+package products;
+
+import base.Pages;
+import org.testng.annotations.Test;
+
+public class CategoryTest extends Pages {
+
+    @Test
+    public void shouldVerifyCategories(){
+        // 1 przeiteruj po wszystki kategoriach
+        // za kazdym razem sprawc czy:
+        // --otowrzyla sie katogira na ktora kliknąłeś
+        // --ilość załadowanych produktów
+        // --jest równa ilości podanych w tyule kategorii
+    }
+}
